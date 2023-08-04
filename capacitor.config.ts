8aba93a73c653d5b7ce0d9ec0988967e80dac80c
Gabriel-aielo <gabriel_aielo@hotmail.com>
2023-08-04 13:55:30 -0300

    AppIonic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aplicativo_de_teste',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ readme.md
@@ -0,0 +1,3 @@
+Este repositório consiste na programação de App utilizando Ionic, com o intuito de conectar uma Esp32 e enviar comandos do App para a Esp32 (e vice versa).
+
+Equipe: Indústria 4.0 do Instituto federal de São Paulo - Campus Guarulhos. 
